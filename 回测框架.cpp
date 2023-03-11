95d3133243928823432ebff3897a223602e643be
Chx_charles <126234692+awwaoeing@users.noreply.github.com>
2023-03-11 16:59:40 +0800

    Update 回测框架.cpp

--- 回测框架.cpp
+++ 回测框架.cpp
@@ -11,7 +11,9 @@ const int N = 365 * 5;
 
 string backtest_scope[N]; //存储回测所用的时间段
 int total_days = 0; //回测所用总天数
+
 //实现将日期转换为时间戳
+
 time_t time_stamp(int year,int month,int day)
 {
     tm t = {};
@@ -23,8 +25,11 @@ time_t time_stamp(int year,int month,int day)
     return ans;
 }
 
-//初始化backtest_scope[N]
-void backtest_scope_init(int sty,int stm,int std,int edy,int edm,int edd) // 参数:回测开始及结束年月日
+
+//初始化backtest_scope[N],存储格式:backtest_scope[k] = 2022-01-01.csv
+//参数: 回测开始及结束的时间
+
+void backtest_scope_init(int sty,int stm,int std,int edy,int edm,int edd)
 {
     //定义开始,结束时间的时间戳
     time_t st = time_stamp(sty,stm,std);
@@ -44,7 +49,9 @@ void backtest_scope_init(int sty,int stm,int std,int edy,int edm,int edd) // 参
     }
 }
 
-//定义可转债结构体,bond[xxx]表序号为xxx 可转债的信息
+
+//定义可转债结构体,bond[xxx]表索引为xxx 可转债的信息
+
 struct Bond
 {
     string name;
@@ -60,14 +67,19 @@ struct Bond
     }
 };
 
+
 //定义仓位
-struct position // 参数: 初始资金及最大容量数量
+
+struct position
 {
     double total_fund;
     double val_cash;
     double val_bond;
-    Bond holding[10];
+    Bond holding[10];  //持仓的可转债
 };
+
+
+//将资金每日的变化写入curve_data.csv 文件
 
 void write_into(string day,position capital)
 {
@@ -78,16 +90,18 @@ void write_into(string day,position capital)
     outfile.close();
 }
 
-//写入第day 天的文件信息
+
+//更新仓位
+
 void update(string day,position capital)
 {
     int cnt = 0; // 记录当天可转债的数量
     Bond bond[300]; // 存储当天可转债的数据
     string line;
     ifstream infile(day);
-
     if (!infile) cout << "Failed to open file!" << endl;
 
+    //将当日的可转债记录在bond[] 中
     while(getline(infile,line))
     {
         stringstream linestream(line);
@@ -101,15 +115,15 @@ void update(string day,position capital)
     }
     infile.close();
 
-    sort(bond,bond + cnt); // 将当天可转债按照factor 从小到大排序
+    sort(bond,bond + cnt); // 将当天所有可转债按照factor 从小到大排序
 
     //预处理已持有和当天的可转债
-    string poi_bond[10];
-    string new_bond[10];
+    string poi_bond[10];  // 已持有的可转债名称
+    string new_bond[10];  // 当天可转债的名称
     for(int i = 0;i < 10;i++) poi_bond[i] = capital.holding[i].name;
     for(int i = 0;i < 10;i++) new_bond[i] = bond[i].name;
 
-    //更新仓位,若仓位中的可转债不再前十则将其卖出
+    //更新仓位,若仓位中的可转债不在前十则将其卖出
     for(int i = 0;i < 10;i++)
     {
         bool ck = 0;
@@ -126,6 +140,7 @@ void update(string day,position capital)
         }
     }
 
+    //更新仓位,将新晋前十的可转债买入
     for(int i = 0;i < 10;i++)
     {
         bool ck = 0;
@@ -139,6 +154,7 @@ void update(string day,position capital)
             for(int z = 0;z < 10;z++) if(capital.holding[z].name == "NULL") capital.holding[z] = {bond[i].name,bond[i].code,bond[i].close,bond[i].premium_rate,bond[i].factor};
         }
     }
+
     capital.total_fund = capital.val_cash + capital.val_bond;
     write_into(day,capital);
 }
@@ -151,6 +167,13 @@ void init_poi(position capital,double f,double c,double b)
     capital.val_bond = b;
 }
 
+//清空curve_data.csv 文件
+void clc()
+{
+    ofstream myfile("curve_data.csv", ios::trunc); //打开文件并清空它
+    myfile.close(); //关闭文件
+}
+
 int main()
 {
     //初始化回测时间段
@@ -160,9 +183,9 @@ int main()
     position capital;
     init_poi(capital,1e6,1e6,0);
 
-    ofstream myfile("curve_data.csv", ios::trunc); //打开文件并清空它
-    myfile.close(); //关闭文件
+    clc();
 
     for(int i = 0;i < total_days;i++) update(backtest_scope[i],capital);
+    return 0;
 }
 
